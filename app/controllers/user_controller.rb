b75f3db9507ccc7e943dea0bb3e7b3c5bc580c52
Thomas Grinstead <tomahawkga@gmail.com>
2020-07-02 15:30:38 -0400

    finished all user controller methods

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,19 +1,24 @@
 class UserController < ApplicationController
 
   def index
+    user = User.find(params[:user_id])
+
+    if user
+      render json: { user: user }
+    else
+      render json: { status: 401 }
+    end
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      render json: {
-        user: user
-      }
+      render json: { user: user }
     end
   end
 
   def login
-    user = User.find_by(email: user_params()["email"]).try(:authenticate, user_params()["password"])
+    user = User.find_by(email: user_params["email"]).try(:authenticate, user_params["password"])
 
     if user
       render json: {
